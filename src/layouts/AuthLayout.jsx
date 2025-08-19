e6995434c5d6c1918d1a17bffa6aca229df95af4
cee-diksha <cee.diksha@gmail.com>
2025-08-19 22:02:46 +0530

    about us page

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,10 +6,10 @@ export default function AuthLayout({ children }) {
   return (
     <div className="min-h-screen bg-white flex flex-col">
       {/* Logo */}
-      <div className="p-6">
+      <div className="p-2">
         <Link to="/">
           <div className="text-2xl flex font-bold text-secondary cursor-pointer capitalize items-center">
-            <img src = {logo} alt="SmileLytics-logo" className="h-20 py-2"/>
+            <img src = {logo} alt="SmileLytics-logo" className="h-20"/>
             SmileLytics
           </div>
         </Link>
