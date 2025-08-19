09f9d32457ff6d0623ec484e555362a42322f282
cee-diksha <cee.diksha@gmail.com>
2025-08-19 21:52:20 +0530

    about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
           
           {/* Text */}
           <p className="text-justify lg:w-2/3">
-            At <span className='text-secondary ml-1 md:font-semibold'> SmileLytics</span>, we believe dentistry should focus more on smiles and less on spreadsheets. That’s why we built an AI-powered dental companion designed to take the heavy lifting out of clinic management — so dentists can focus on what they do best: <span className='text-secondary ml-1 md:font-semibold'>caring for patients</span>.
+            At <span className='text-secondary ml-1 font-semibold'> SmileLytics</span>, we believe dentistry should focus more on smiles and less on spreadsheets. That’s why we built an AI-powered dental companion designed to take the heavy lifting out of clinic management — so dentists can focus on what they do best: <span className='text-secondary ml-1 font-semibold'>caring for patients</span>.
             <br /><br />
             Our assistant is like your digital chairside partner — always attentive, always reliable, and always learning. From managing patients to keeping track of visits, payments, and reminders, it makes running a dental practice smoother, smarter, and stress-free.
           </p>
@@ -64,7 +64,7 @@ const About = () => {
         </div>
 
         {/* Why We Built It Section */}
-        <div className= "py-8 px-4 md:px-10 w-full mt-10 mb-16">
+        <div className= "py-8 px-4 md:px-10 w-full mt-5 mb-16">
           <h4 className="text-textdark font-bold text-xl md:text-2xl">
             Why We Built It
           </h4>
